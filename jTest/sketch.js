d913c73ed6fbbdebc7c5cc11d5c6bbe15649f81f
Nick Puckett <nick@altnresearch.com>
2019-01-28 14:45:14 -0500

    s

--- jTest/sketch.js
+++ jTest/sketch.js
@@ -2,7 +2,7 @@ var askurl = 'https://qrng.anu.edu.au/API/jsonI.php?length=10&type=uint8';
 
 var ask2 = 'https://api.wolframalpha.com/v1/result?i=Is+a+tomato+a+fruit%3F&appid=GY4UW4-ATAR842QWR&format=image&output=JSON';
 
-var ask3 = 'http://api.wolframalpha.com/v1/conversation.jsp?appid=GY4UW4-ATAR842QWR&i=How+tall+is+michael+jordan%3f&units=metric'
+var ask3 = 'https://api.wolframalpha.com/v1/conversation.jsp?appid=GY4UW4-ATAR842QWR&i=How+tall+is+michael+jordan%3f&units=metric'
 
 function setup()
 {
